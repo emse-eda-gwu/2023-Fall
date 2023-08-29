7746e01ff30cf32481369113dbb07b3157cc2a42
emse-p4a-gwu <john.helveston@gmail.com>
2023-08-29 09:16:21 -0400

    Update render.R

--- class/render.R
+++ class/render.R
@@ -5,21 +5,16 @@ lesson <- lesson[length(lesson)]
 renderthis::to_html("index.Rmd", "index.html")
 renderthis::to_pdf("index.html", paste0(lesson, ".pdf"))
 
+# Compress the PDF to reduce size
+tools::compactPDF(paste0(lesson, ".pdf"), gs_quality = 'ebook')
+
 # Create zip files of class notes
 zip::zip(
     zipfile = paste0(lesson, ".zip"),
     files = c(
         'data',
-        'caseConverter_solution.R',
-        'caseConverter.R',
-        'federalSpending.R',
-        'federalSpendingCompare.R',
-        'html.R',
-        'internetUsers_solution.R',
-        'internetUsers.R',
-        'markdown.R',
-        'mpg.R',
-        'shinyWidgets.R',
-        'widgets.R',
+        'practice.qmd',
+        'practice-solutions.qmd',
+        'quarto_demo.qmd',
         paste0(lesson, ".Rproj"))
 )
